047e12324ef8ad3e8717323764ab2ddd5b82e287
Jyotirchemate <Jyotirchemate@gmail.com>
2025-03-28 14:53:31 +0530

    test1 file updated

--- Test1.py
+++ Test1.py
@@ -1,13 +1,13 @@
 class Test1:
     def sayHello(self):
-        print("hello from class test1")
+        print("hello from class new test1")
     def sayHi(self):
-        print("hi from class test1")
+        print("hi from class new test1")
 class Test2:
 
 
-    x=5
-    y=10
+    x=10
+    y=20
     def sayHello(self):
         print("hello from class test2")
     def sayHi(self):
